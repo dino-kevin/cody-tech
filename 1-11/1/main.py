33fe20d865567e8bcc894817ae23a8db227675d4
Kevin Castaneda <pollopower74@gmail.com>
2026-02-19 13:39:54 -0600

    Added chapter 11 example and first challenge

---
+++ 1-11/1/main.py
@@ -0,0 +1,2 @@
+shopping_list = ["bread", "eggs", "milk", "butter"]
+print(f"shopping_list = {shopping_list}")
---
+++ 1-11/example.py
@@ -0,0 +1,3 @@
+my_list = [1, 2, "three", True]
+length = len(my_list)
+print (f"my list is: {my_list} and the lenght is {length}")
