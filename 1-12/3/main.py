e1ca10f7efc7d3f471817e4b6328032137b01132
Kevin Castaneda <pollopower74@gmail.com>
2026-02-21 17:06:48 -0600

    Added chapter 12 : subchapter 3

---
+++ 1-12/3/main.py
@@ -0,0 +1,12 @@
+text = input()
+# Write your code below
+
+def find_char(text, letter):
+    count = 0
+    for index, char in enumerate(text):
+        if letter in char:
+            count+= 1
+    return count        
+
+found_char = find_char(text.lower(), "p")
+print(f"{found_char}")
