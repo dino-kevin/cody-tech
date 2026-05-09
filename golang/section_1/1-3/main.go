8a59b043248d431149f2dd645543fa54ab835b1d
Kevin Castaneda <pollopower74@gmail.com>
2026-05-08 22:58:19 -0600

    Added golang: Section 1: Subchapter 3

---
+++ golang/section_1/1-3/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: Use fmt.Println() to print "Alex 25 coding" on a single line
+	// Hint: You can provide multiple arguments to fmt.Println()
+	fmt.Println("Alex", 25, "coding")
+}
