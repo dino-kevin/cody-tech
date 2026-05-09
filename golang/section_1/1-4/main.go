22f519ccee5a8d565919865dcf5d5adc5496ca18
Kevin Castaneda <pollopower74@gmail.com>
2026-05-08 23:08:02 -0600

    Added golang: Section 1: Subchapter 4

---
+++ golang/section_1/1-4/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    // TODO: Add a single-line comment here explaining that this program displays a welcome message
+    // This will print: "Welcome to Go programming!"
+    
+    // TODO: Add a multi-line comment here explaining that we're using fmt.Println to display text
+    /*
+        We are using fmt.Println in order to print the message Welcome to go programming!
+    */
+    
+    fmt.Println("Welcome to Go programming!")
+}
