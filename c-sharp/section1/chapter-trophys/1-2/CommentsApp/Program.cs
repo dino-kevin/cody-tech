d639a05b9f1ff5f3284c83ded300280a2e1e300b
dino-kevin <pollopower74@gmail.com>
2026-06-26 22:06:45 -0600

    add/C#: Section 1 Program 2

---
+++ c-sharp/section1/chapter-trophys/1-2/CommentsApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Program {
+    public static void Main(string[] args) {
+        // Type your code below
+        // Console.WriteLine("Goodbye!");
+        Console.WriteLine("Hello, C#!");
+    }
+}
