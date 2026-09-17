162745aea615b130d3af5d8488f6689fd67b63e7
dino-kevin <pollopower74@gmail.com>
2026-09-17 08:17:53 -0600

    Fixed the location of the projects of golang: Section 1: Chapter 10

---
+++ c-sharp/section1/chapter-trophys/9-1/1/Recap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        // Sum of all numbers
+        for (int i = 1; i <= count; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            sum += number;
+        }
+        Console.WriteLine($"{sum}");
+
+    }
+
+}
